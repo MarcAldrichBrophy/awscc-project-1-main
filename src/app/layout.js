32a522cbcf88f7e74a74e08a9112be99c0fa93eb
Carter Martin <97261521+Carter-Martin@users.noreply.github.com>
2023-11-13 22:35:02 -0800

    Fixed hydration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import styles from './page.module.css'
-import Navbar from './components/Navbar'
+import Navbar from './Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,11 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <div className={styles.main} id="mainHeader">
-        <Navbar/>
-
-
-      </div>
       <body className={inter.className}>{children}</body>
     </html>
   )
