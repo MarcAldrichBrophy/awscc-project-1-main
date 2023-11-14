b885f3993d96f19af1709da82a742397f21d8579
Carter Martin <97261521+Carter-Martin@users.noreply.github.com>
2023-11-14 01:01:39 -0800

    Removed page-specific navbar

--- src/app/page.js
+++ src/app/page.js
@@ -6,41 +6,24 @@ import computers from './images/computerDiagram.png'
 
 export default function Home() {
   return (
-    <div>
-      <header className={styles.header}>
-        <div className={styles.logoContainer}>
-          <Image src="/logo.png" alt="Logo" width={75} height={60} />
-          <h1 className={styles.logoTitle}>Extracta</h1>
-        </div>
-        <nav className={styles.nav}>
-          <a href="/" className={styles.navItem}>Home</a>
-          <a href="/about" className={styles.navItem}>About</a>
-          <Link href="/services" className={styles.navButton}>Try Now
-          </Link>
-        </nav>
-      </header>
+    <main className={styles.main}>
+      <div className={styles.contentContainer}>
+        <h1 className={styles.headline}>Unlock value from meetings, documents, and visual data.</h1>
+        <a href="/services" className={styles.tryNowButton}>Try Now</a>
+      </div>
+      <div className={styles.illustration}>
+        <Image
+          src={computers}
+          alt="Illustration"
+          width={400} // set the appropriate size
+          height={400}
+          style={{
+            position: "absolute"
+          }}
+          priority
+        />
+      </div>
+    </main>
 
-      <main className={styles.main}>
-        <div className={styles.contentContainer}>
-            <h1 className={styles.headline}>Unlock value from meetings, documents, and visual data.</h1>
-            <a href="/services">
-              <button className={styles.tryNowButton}>Try Now</button>
-            </a>
-            
-        </div>
-        <div className={styles.illustration}>
-          <Image
-            src={computers}
-            alt="Illustration"
-            width={400} // set the appropriate size
-            height={400}
-            style={{
-              position: "absolute"
-            }}
-            priority
-          />
-        </div>
-      </main>
-    </div>
   );
 }
--- src/app/services/page.js
+++ src/app/services/page.js
@@ -6,21 +6,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <header className={styles.header}>
-        <div className={styles.logoContainer}>
-          <Image src="/logo.png" alt="Logo" width={75} height={60} />
-          <h1 className={styles.logoTitle}>Extracta</h1>
-        </div>
-        <nav className={styles.nav}>
-          <a href="/" className={styles.navItem}>Home</a>
-          <a href="/about" className={styles.navItem}>About</a>
-          <Link href="/services" className={styles.navButton}>Try Now
-          </Link>
-        </nav>
-      </header>
-
       <main className={styles.main}>
-       
       </main>
     </div>
   );
