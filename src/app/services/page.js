73bc092c7e6be1748740b18f170e3373f431684b
Carter Martin <97261521+Carter-Martin@users.noreply.github.com>
2023-11-14 02:41:06 -0800

    Added cards and redirects to services page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,12 +1,39 @@
-// Home page
+'use client'
 import Image from 'next/image';
 import styles from './page.module.css';
 import Link from 'next/link';
+import Button from '../components/Button';
 
 export default function Home() {
+  const click = (page) => {
+    console.log("click");
+    // Logging code goes here
+    if(page == 1) window.location.href = '/services/rekognition';
+    else if (page == 2) window.location.href = '/services/transcribe';
+    else if (page == 3) window.location.href = '/services/comprehend';
+    else window.location.href = '/';
+    // Then redirect
+
+  };
+
   return (
     <div>
-      <main className={styles.main}>
+      <main className={styles.Container}>
+        <div className={styles.Card}>
+          <div className={styles.Title}>Rekognition</div>
+          <div className={styles.Text}>Extract items in images to turn visual data into tangible insights</div>
+          <Button name={"Use"} cls={styles.Button} onClickHandler={() => click(1)}/>
+        </div>
+        <div className={styles.Card}>
+          <div className={styles.Title}>Transcribe</div>
+          <div className={styles.Text}>Extract text from meetings and redact personal identifiable information</div>
+          <Button name={"Use"} cls={styles.Button} onClickHandler={() => click(2)}/>
+        </div>
+        <div className={styles.Card}>
+          <div className={styles.Title}>Comprehend</div>
+          <div className={styles.Text}>Extract data, topics, and classifications with confidence scores from your documents</div>
+          <Button name={"Use"} cls={styles.Button} onClickHandler={() => click(3)}/>
+        </div>
       </main>
     </div>
   );
